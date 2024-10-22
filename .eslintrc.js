4c06e3522f6ab7bda15f085af2cd4b81a97be408
hieucuopbien123 <mac@192.168.101.20>
2024-10-22 22:40:30 +0700

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   // extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true, 
     },
     ecmaVersion: "latest",
     sourceType: "module",
