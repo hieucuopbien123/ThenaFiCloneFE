36033a6a316288c02994eae9306c6626c631aa69
hieucuopbien123 <mac@192.168.101.20>
2024-10-22 21:24:47 +0700

    A

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  // extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -23,15 +23,15 @@ module.exports = {
     "linebreak-style": 0,
     quotes: ["error", "double"],
     semi: ["error", "always"],
-    "no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_$",
-        argsIgnorePattern: "^_$",
-        ignoreRestSiblings: true,
-      },
-    ],
+    // "no-unused-vars": [
+    //   "warn",
+    //   {
+    //     vars: "all",
+    //     varsIgnorePattern: "^_$",
+    //     argsIgnorePattern: "^_$",
+    //     ignoreRestSiblings: true,
+    //   },
+    // ],
     "no-console": "off",
     "no-prototype-builtins": "off",
     "no-empty": "warn",
